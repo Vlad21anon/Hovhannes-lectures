419fe0c5c965ae5a6d701f2d085f8f216768687c
Vlad21anon <103931385+Vlad21anon@users.noreply.github.com>
2022-07-19 20:41:37 +0300

    Add files via upload

---
+++ ALL Lectures/Duck.java
@@ -0,0 +1,7 @@
+package lecturesHomework.L21.E22;
+
+public class Duck extends Animal{
+    public Duck(String name,String move){
+        super(name,move);
+    }
+}
---
+++ ALL Lectures/Fish.java
@@ -0,0 +1,8 @@
+package lecturesHomework.L21.E22;
+
+public class Fish extends Animal {
+
+    public Fish(String name,String move){
+        super(name,move);
+    }
+}
