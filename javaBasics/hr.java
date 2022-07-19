dbc2a52022f428a03226502615989610cb4c5398
Vlad21anon <103931385+Vlad21anon@users.noreply.github.com>
2022-07-19 21:03:10 +0300

    Delete javaBasics directory

--- javaBasics/hr.java
+++
@@ -1,9 +0,0 @@
-package javaBasics;
-
-public class hr {
-    public static void hr(String args) {
-        Main.print(args + ", вы, к сожалению, не приняты. " +
-                "У вас мало опыта");
-    }
-}
-
--- javaBasics/manager.java
+++
@@ -1,9 +0,0 @@
-package javaBasics;
-
-import javaBasics.Main;
-
-public class manager {
-    public static void manager(String args) {
-        Main.print("Здравствуйте, " + args + ". Следуйте за мной");
-    }
-}
