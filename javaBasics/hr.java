999b7b95187fd3c3698d04d6a2fc0432c98622cf
Vlad21anon <103931385+Vlad21anon@users.noreply.github.com>
2022-07-19 13:06:34 +0300

    Первые файлы загруженные на ГитХаб
    Старт на джаве. psvm, sout

---
+++ javaBasics/hr.java
@@ -0,0 +1,9 @@
+package javaBasics;
+
+public class hr {
+    public static void hr(String args) {
+        Main.print(args + ", вы, к сожалению, не приняты. " +
+                "У вас мало опыта");
+    }
+}
+
---
+++ javaBasics/manager.java
@@ -0,0 +1,9 @@
+package javaBasics;
+
+import javaBasics.Main;
+
+public class manager {
+    public static void manager(String args) {
+        Main.print("Здравствуйте, " + args + ". Следуйте за мной");
+    }
+}
